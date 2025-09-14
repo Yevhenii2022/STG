0732b4b5f118330184f17598abf9fb8bd88fae1b
Yevhenii2022 <3starosta3@gmail.com>
2025-09-14 14:24:26 +0300

    add

--- inc/custom-post-type.php
+++ inc/custom-post-type.php
@@ -1,2 +1,69 @@
 <?php
 
+function people_custom_post()
+{
+  // Реєстрація кастомної таксономії
+  register_taxonomy('people-type', ['people'], [
+    'labels' => [
+      'name' => 'Категорії',
+      'singular_name' => 'Категорія',
+      'search_items' => 'Шукати категорії',
+      'all_items' => 'Усі категорії',
+      'view_item' => 'Переглянути категорію',
+      'parent_item' => 'Батьківська категорія',
+      'parent_item_colon' => 'Батьківська категорія:',
+      'edit_item' => 'Редагувати категорію',
+      'update_item' => 'Оновити категорію',
+      'add_new_item' => 'Додати нову категорію',
+      'new_item_name' => 'Назва нової категорії',
+      'menu_name' => 'Категорії',
+      'back_to_items' => '← Назад до категорій',
+    ],
+    'public' => true,
+    'hierarchical' => true,
+    'rewrite' => [
+      'slug' => 'people-category',
+      'with_front' => true,
+      'hierarchical' => false,
+    ],
+    'show_admin_column' => true,
+    'show_in_rest' => true,
+  ]);
+
+  // Labels для кастомного пост-тайпу
+  $labels = array(
+    'name' => esc_html__('Люди', 'stg'),
+    'singular_name' => esc_html__('Людина', 'stg'),
+    'add_new' => esc_html__('Додати нову людину', 'stg'),
+    'add_new_item' => esc_html__('Додати нову людину', 'stg'),
+    'edit_item' => esc_html__('Редагувати людину', 'stg'),
+    'new_item' => esc_html__('Нова людина', 'stg'),
+    'all_items' => esc_html__('Усі люди', 'stg'),
+    'view_item' => esc_html__('Переглянути людину', 'stg'),
+    'search_items' => esc_html__('Шукати людей', 'stg'),
+    'not_found' => esc_html__('Людей не знайдено', 'stg'),
+    'featured_image' => esc_html__('Фото', 'stg'),
+    'set_featured_image' => esc_html__('Встановити фото', 'stg')
+  );
+
+  // Аргументи для кастомного пост-тайпу
+  $args = array(
+    'labels' => $labels,
+    'description' => 'Люди',
+    'public' => true,
+    'menu_position' => 20,
+    'show_in_rest' => true,
+    'supports' => array('title', 'thumbnail'),
+    'has_archive' => false,
+    'show_in_admin_bar' => true,
+    'show_in_nav_menus' => true,
+    'rewrite' => array('slug' => 'people'),
+    'menu_icon' => 'dashicons-groups',
+    'query_var' => true,
+    'taxonomies' => array('people-type')
+  );
+
+  register_post_type('people', $args);
+}
+
+add_action('init', 'people_custom_post');
--- template-parts/people-section.php
+++ template-parts/people-section.php
@@ -4,9 +4,59 @@ $name = get_field('people') ?? '';
 
 <section id="people" class="people">
   <div class="container">
-    <div class="people__wrapper d-flex flex-column">
+    <div class="people__wrapper">
       <?php if ($name) : ?>
         <h3><?= $name ?></h3>
+      <?php endif; ?>
+
+
+
+      <?php if ($post_type == 'post'): ?>
+
+        <div class="sort-wrapper">
+          <div class="search-input">
+            <input type="text" id="search-input" placeholder="Search by article...">
+          </div>
+          <div class="select">
+            <input class="select__input" type="hidden" name="" id="sort-order">
+            <div class="select__head">Sort by:</div>
+            <ul class="select__list" style="display: none;">
+              <li class="select__item" data-sort="DESC">From Newest to Oldest</li>
+              <li class="select__item" data-sort="ASC">From Oldest to Newest</li>
+            </ul>
+          </div>
+        </div>
+
+        <?php if ($category_query->have_posts()) : ?>
+          <div class="post-query" id="post-list">
+            <?php while ($category_query->have_posts()) : $category_query->the_post(); ?>
+              <div class="single-post-item">
+                <a href="<?php echo get_permalink(); ?>">
+                  <div>
+                    <?php the_post_thumbnail(); ?>
+                    <h4><?php echo get_the_title(); ?></h4>
+                  </div>
+                  <div class="post-wrapper">
+                    <p class="post-date"><?php echo get_the_date('j F Y'); ?></p>
+                    <p class="post-text"><?php echo get_the_excerpt(); ?></p>
+                    <span>Read now</span>
+                  </div>
+                </a>
+              </div>
+            <?php endwhile; ?>
+          </div>
+
+          <?php wp_reset_postdata(); ?>
+
+        <?php else: ?>
+          <p class="post-title">There are currently no posts</p>
+        <?php endif; ?>
+
+        <?php if ($category_query->max_num_pages > 1) : ?>
+          <div id="load-more-posts" class="wp-block-button is-style-outline-custom-button" data-post-type="<?php echo $post_type ?>" data-page="1" data-totalpages="<?php echo $category_query->max_num_pages; ?>">
+            <a class="wp-block-button__link wp-element-button">Load more</a>
+          </div>
+        <?php endif; ?>
       <?php endif; ?>
 
 
